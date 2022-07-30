d96d1446de8fff2be5466ffa6a9443cb37f86460
Sam Lord <samlord6@gmail.com>
2022-07-30 17:40:57 +0100

    Socket comms

---
+++ Socketeering/Gateway.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+
+namespace Socketeering
+{
+    public class Gateway
+    {
+        private readonly int PORT;
+        // Multicast range is 224.0.0.0-239.255.255.255 
+        private readonly IPAddress MULTICAST_ADDRESS;
+
+        private readonly Socket sendSocket;
+        private readonly Socket recvSocket;
+
+        public Gateway(int port, IPAddress multicastAddress)
+        {
+            PORT = port;
+            MULTICAST_ADDRESS = multicastAddress;
+
+            sendSocket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+            recvSocket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+
+            Connect();
+        }
+
+        public void Connect()
+        {
+            ConnectSend();
+            ConnectRecv();
+        }
+
+        private void ConnectRecv()
+        {
+            if (recvSocket.Connected) return;
+            // Bind socket to multicast endpoint
+            IPEndPoint ipep = new IPEndPoint(IPAddress.Any, PORT);
+            recvSocket.Bind(ipep);
+            
+            // Join multicast group
+            recvSocket.SetSocketOption(SocketOptionLevel.IP,SocketOptionName.AddMembership, new MulticastOption(MULTICAST_ADDRESS, IPAddress.Any));
+        }
+
+        private void ConnectSend()
+        {
+            if (sendSocket.Connected) return;
+
+            // join multicast group
+            sendSocket.SetSocketOption(SocketOptionLevel.IP, SocketOptionName.AddMembership, new MulticastOption(MULTICAST_ADDRESS));
+
+            // TTL, 1 = within local network (1 hop)
+            sendSocket.SetSocketOption(SocketOptionLevel.IP, SocketOptionName.MulticastTimeToLive, 2);
+            
+            IPEndPoint multicastEndpoint = new IPEndPoint(MULTICAST_ADDRESS, PORT);
+
+            // Connect to the multicast endpoint
+            sendSocket.Connect(multicastEndpoint);
+        }
+
+        public void CloseAll()
+        {
+            sendSocket.Close();
+            recvSocket.Close();
+        }
+
+        public void SendSync(string message)
+        {
+            byte[] messageBytes = Encoding.UTF8.GetBytes(message);
+            sendSocket.Send(messageBytes, messageBytes.Length, SocketFlags.None);
+        }
+
+        public async void Send(string message)
+        {
+            Task sendTask = new Task(() =>
+            {
+                SendSync(message);
+            });
+            sendTask.Start();
+            await sendTask;
+        }
+
+        public Task RecvMessageContinuous(Action<string> onMessage)
+        {
+            Task recvTask = new Task(() =>
+            {
+                while (true)
+                {
+                    string msg = RecvMessageSync();
+                    onMessage(msg);
+                }
+            });
+            recvTask.Start();
+            return recvTask;
+        }
+
+        //public IEnumerable<string> RecvMessageContinuous()
+        //{
+        //    while (true)
+        //    {
+        //        string message = RecvMessageSync();
+        //        yield return message;
+        //    }
+        //}
+
+        public string RecvMessageSync()
+        {
+            string? str;
+
+            do
+            {
+                byte[] incomingBuffer = new byte[1024];
+                recvSocket.Receive(incomingBuffer);
+                str = Encoding.ASCII.GetString(incomingBuffer, 0, incomingBuffer.Length).Trim();
+            } while (str.Length == 0);
+
+            return str;
+        }
+
+        public async Task<string> RecvMessage()
+        {
+            Task<string> recvTask = new Task<string>(() =>
+            {
+                string str = RecvMessageSync();
+                return str;
+            });
+            await recvTask;
+            return recvTask.Result;
+        }
+    }
+}
---
+++ Socketeering/Program.cs
@@ -0,0 +1,42 @@
+﻿using Socketeering;
+using System.Net;
+
+public class Program
+{
+    private static readonly int PORT = 4327;
+    private static readonly IPAddress MULTICASTADDRESS = IPAddress.Parse("238.7.6.5");
+
+    public static void Main(string[] args)
+    {
+        Console.WriteLine($"Hello, I am {NameGenerator.GetName()}");
+        Gateway gateway = new Gateway(PORT, MULTICASTADDRESS);
+
+        Task sendTask = InteractiveSend(gateway);
+        sendTask.Start();
+
+        gateway.RecvMessageContinuous((string message) =>
+        {
+            Console.WriteLine("Incoming>>");
+            Console.WriteLine(message);
+            Console.WriteLine("<<End");
+        });
+        sendTask.Wait();
+    }
+
+    public static Task InteractiveSend(Gateway gateway)
+    {
+        return new Task(() =>
+        {
+            while (true)
+            {
+                Console.Write(">");
+                string? input = Console.ReadLine();
+                if (input == "_CLOSE") return;
+                if (input == null) throw new Exception("NULL input from console");
+
+                gateway.Send(input);
+            }
+        });
+    }
+
+}
